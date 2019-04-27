20a628f11dd2b4d8d90763b1684fe5605a4b8394
Alex Wong <fi0088@wayne.edu>
2019-04-27 07:32:50 -0700

    Options route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard';
 import Auth from './components/auth/Auth';
 import User from './components/user/User';
+import Options from './components/options/Options';
 
 const App = () => {
   return (
     <Router>
       <Route exact path="/" component={Dashboard} />
       <Route path="/user/:user" component={User} />
+      <Route path="/options" component={Options} />
       <Route path="/login" component={Auth} />
       <Route path="/register" component={Auth} />
     </Router>
