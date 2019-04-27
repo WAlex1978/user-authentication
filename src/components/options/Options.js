176bad64aaed423b4c5b6209d7b8502a5fbcd338
Alex Wong <fi0088@wayne.edu>
2019-04-27 07:32:37 -0700

    Options list

---
+++ src/components/options/Options.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Wrapper, Flex, Text } from '../styled-components';
+
+import PropTypes from 'prop-types';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = () => ({
+    List: {
+        width: "20%",
+    },
+    classList: {
+        "&:hover": {
+            backgroundColor: "rgb(254, 174, 186)",
+        }
+    },
+    Selected: {
+        backgroundColor: "rgb(254, 74, 86) !important",
+        color: "white",
+    }
+});
+
+class Options extends Component {
+    state = {
+        selected: 0,
+    }
+
+    handleClick = (index) => {
+        this.setState({selected: index});
+    }
+    
+    render() { 
+        const { classes } = this.props;
+
+        return (
+            <Wrapper>
+                <Flex>
+                    <List className={classes.List}>
+                        <ListItem button selected={this.state.selected===0} onClick={() => this.handleClick(0)} className={classes.classList} classes={{selected: classes.Selected}}>
+                            <Text color="inherit">User Settings</Text>
+                        </ListItem>
+                        <ListItem button selected={this.state.selected===1} onClick={() => this.handleClick(1)} className={classes.classList} classes={{selected: classes.Selected}}>
+                            <Text color="inherit">Profile Settings</Text>
+                        </ListItem>
+                        <ListItem button selected={this.state.selected===2} onClick={() => this.handleClick(2)} className={classes.classList} classes={{selected: classes.Selected}}>
+                            <Text color="inherit">Profile Picture</Text>
+                        </ListItem>
+                        <ListItem button selected={this.state.selected===3} onClick={() => this.handleClick(3)} className={classes.classList} classes={{selected: classes.Selected}}>
+                            <Text color="inherit">Following</Text>
+                        </ListItem>
+                    </List>
+                    <div style={{flex: 1}}></div>
+                </Flex>
+            </Wrapper>
+        );
+    }
+}
+
+Options.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+ 
+export default withStyles (styles) (Options);
