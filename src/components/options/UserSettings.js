0f6a2fb80e5ea373562b7601389bb17a1778e12c
Alex Wong <fi0088@wayne.edu>
2019-05-16 08:44:38 -0700

    User Settings Page Divider

--- src/components/options/UserSettings.js
+++ src/components/options/UserSettings.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text } from '../styled-components';
+import Divider from '@material-ui/core/Divider';
 
 const UserSettings = () => {
     return (
         <div>
             <Text bottom="10px">User Settings</Text>
+            <Divider />
         </div>
     )
 }
